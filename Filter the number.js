31b3e732e9440d63bedc8980d5a11fa52091b6a0
Nicolukas12345 <velozapinzonjosenicolas@gmail.com>
2026-07-23 16:40:24 -0500

    Had to check the solution but I understand and will add it to my anki

---
+++ Filter the number.js
@@ -0,0 +1,31 @@
+/*
+Filter the number
+
+Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+Task
+
+Your task is to return a number from a string.
+Details
+
+You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+*/
+
+//PREP
+/*
+P: string mix of letters and numbers
+R: integer
+E: input: "a1b2c3" output: 123
+P: extractNum (string)
+    result = string.split("").filter((element) => element.isInteger())
+    return Number(result);
+*/
+
+
+function filterString(value) {
+  let result = "";
+  for (i of value){
+    if (!isNaN(i)){
+      result += i;
+    }
+  }
+  return (Number(result)); }
