5dde5471c6dc0c95e5724ac0a70d833a92b80e65
Nicolukas12345 <velozapinzonjosenicolas@gmail.com>
2026-08-31 22:29:32 +0000

    took way too long but finally did it

---
+++ Consecutive strings.js
@@ -0,0 +1,63 @@
+/*
+You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+Examples:
+
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+Note
+
+consecutive strings : follow one after another without an interruption
+*/
+
+//PREP
+/*
+P: string and integer
+R: string
+E: input = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"] k = 2  ["treefoling":10, "folingtrashy":12, "trashyblue":10, "blueabcdef":10, "abcdefuvwxyz":12] output: "folingtrashy"
+P: longestConsec(starr, k)
+    buffer = []
+    base = ""
+    longest = 0
+    loop index of starr.length - k
+      base = ""
+      loop index < k
+        base += starr[index]
+      if (base.length > longest)
+        longest = base.length
+    return(buffer.find((element) => element.length === longest))
+*/
+function longestConsec(strarr, k) {
+  let buffer = [];
+  let base;
+  let longest = 0;
+  if (strarr.length === 0 || k > strarr.length || k <= 0){
+    return "";
+  }
+  for (let i = 0; i < strarr.length - (k -1); i++){
+    base = "";
+    for (let j = i; j < k + i; j++){
+      base += strarr[j];
+    }
+    buffer.push(base);
+    if (base.length > longest){
+      longest = base.length;
+    }
+  }
+  return (buffer.find((element) => element.length === longest));
+}
